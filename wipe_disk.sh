f22391ff0356f24301e70446cb3b810cc0d3b942
User Name <user@localhost>
2025-06-03 15:09:43 -0500

    Tue Jun  3 15:09:43 CDT 2025

---
+++ wipe_disk.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# variables
+################################################################################
+
+# functions
+################################################################################
+f_help () {
+	echo "
+This script wipes out (erases all data) on a given disk.
+
+SYNTAX:
+  # $0 <Disk/Device> [OPTIONS]
+
+OPTIONS:
+  -h, --help    display this help text
+  -w, --wipe    wipe disk
+
+EXAMPLE:
+  # $0 /dev/sda -w
+
+" ;exit
+}
+
+# script start
+################################################################################
+
+# input
+########################################
+
+# check package manager
+if [[ `apt` ]] ;then
+	PCKMNG='apt'
+elif [[ `yum` ]] ;then
+	PCKMNG='yum' ;fi
+echo $PCKMNG
+
+# install software
+if [[ ! `which dd` ]] ;then
+	$PCKMNG install -y dd ;fi
+
+if [[ ! `which shred` ]] ;then
+	$PCKMNG install -y core-utils ;fi
+
+# identify disk
+DISK="$1"
+
+# processing
+########################################
+
+# error check: disk
+if [[ ! `grep dev $DISK` ]] ;then
+	echo "$DISK is not a device." ;exit ;fi
+
+exit
+# zero out disk
+dd if=/dev/zero of=$DISK
+
+# wipe disk w/utility
+# -v   verbose
+# -n1  wipe disk once randomly
+# -z   wipa disk again with all zeroes
+shred -vzn1 $DISK
+
+# output
+########################################
+
+# display results
+lsblk
