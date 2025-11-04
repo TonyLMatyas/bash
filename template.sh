9cb3c8eb73ffe5bdfd42c5c9c29206f24d1bc32f
user <user@localhost>
2025-11-04 17:45:21 -0600

    mod display

--- template.sh
+++ template.sh
@@ -5,7 +5,23 @@
 
 # Variables
 ########################################
-#FLLPTH="`readlink -e $0`"  # The full path to this script (basepath)
+
+# Default
+BASE="`readlink -f $0`"  # The full path to this script (basepath)
+HOME=~
+DATE="`date +%F`"
+
+# Backup Variables
+VBNM="custom"
+VBND="$VBNM/$DATE"
+DBMN="$HOME/backups/$VBND"
+
+# Working Variables
+DWMN="/tmp/$VBND"
+FWTO="$DWMN/tempone.tmp"
+FWTT="$DWMN/temptwo.tmp"
+
+# Custom
 
 # Functions
 ########################################
@@ -31,10 +47,23 @@ NOTES:
 # Print error message & help text
 f_errr () { echo ;echo "!!! ERROR: $1 !!!" ;f_hlp ; }
 
-# Display
-f_display () { echo "
-=
-" ; }
+# If needed, make directory
+f_ifdir () { if [[ ! -d "$1" ]] ;then mkdir -p "$1" ;fi ; }
+
+# Initialize Files/Directories
+f_initdirs () {
+	cat /dev/null > $FWTO
+	cat /dev/null > $FWTT
+	f_ifdir "$DBMN"
+	f_ifdir "$DWMN" ; }
+
+# Display Variables
+f_display () {
+	f_initdirs
+	declare -p > $FWTO
+	for var in `grep '=' $FWTO |grep '^[A-Z]' |awk -F'=' '{print $1}'` ;do
+		if [[ `grep "$var" "$BASE"` ]];then
+			grep ^$var\= $FWTO ;fi ;done ; }
 
 # Arguments
 ########################################
