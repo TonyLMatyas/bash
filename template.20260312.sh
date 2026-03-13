7577822d59385be1803052b1c9391d1e9ce5ef7b
user <user@localhost>
2026-03-12 20:32:56 -0500

    fix template

---
+++ template.20260312.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+# Input
+################################################################################
+
+# Variables
+########################################
+
+# Default Variables
+HOME=~
+FULL="`readlink -f $0`"  # The full path to this script (basepath)
+BASE="`basename $0`"
+DATE="`date +%F`"
+
+# Backup Variables
+VBND="$BASE/$DATE"
+DBMN="$HOME/backups/$VBND"
+
+# Working Variables
+DWMN="/tmp/$VBND"
+FWTO="$DWMN/tempone.tmp"
+FWTT="$DWMN/temptwo.tmp"
+
+# Custom Variables
+
+# Functions
+########################################
+
+# Display help text
+f_hlp () { echo "
+DESCRIPTION:
+  This script ...
+
+SYNTAX:
+  # $BASE [OPTIONS]
+
+OPTIONS:
+  -h, --help
+    Displays this help text.
+  -r, --run
+    Executes this script.
+
+EXAMPLES:
+  # $BASE -h
+
+NOTES:
+  ...
+" ; exit ; }
+
+# Print error message & help text
+f_errr () { echo ;echo "!!! ERROR: $1 !!!" ;f_hlp ; }
+
+# If needed, make directory
+f_ifdir () { if [[ ! -d "$1" ]] ;then mkdir -p "$1" ;fi ; }
+
+# Initialize files/directories
+f_initdirs () {
+  f_ifdir "$DBMN"
+  f_ifdir "$DWMN"
+  cat /dev/null > $FWTO
+  cat /dev/null > $FWTT ; }
+
+# Print message with dots
+f_dots () {
+  echo ;echo -n "$1"
+  for dots in {1..5} ;do
+    sleep 0.2 ;echo -n '.' ;done ;echo ; }
+
+# Display variables
+f_display () {
+  f_initdirs
+  exit # need to check compatability on linux & mac
+  declare -p > $FWTO
+  for var in `grep '=' $FWTO |grep '^[A-Z]' |awk -F'=' '{print $1}'` ;do
+    if [[ `grep "$var" "$FLLP"` ]];then
+      grep ^$var\= $FWTO ;fi ;done ; }
+
+# Arguments
+########################################
+
+# Filter options
+case $1 in
+  '-r'|'--run')  RUN='true' ;shift  ;;
+  *)  HELP='true' ;shift  ;;
+esac
+
+# Processing
+################################################################################
+
+# Error Checks
+########################################
+
+# Check for help
+if [[ $HELP == 'true' ]] ;then f_hlp ;fi
+
+# Check for root privilege execution
+#if [[ `whoami` != 'root' ]] ;then f_errr 'use "sudo" for execution' ;fi
+
+# Script Start
+########################################
+
+# Process options
+if [[ $RUN != 'true' ]] ;then
+  exit
+fi
+
+# Output
+################################################################################
+
--- template.sh
+++ template.sh
@@ -6,23 +6,6 @@
 # Variables
 ########################################
 
-# Default Variables
-HOME=~
-FULL="`readlink -f $0`"  # The full path to this script (basepath)
-BASE="`basename $0`"
-DATE="`date +%F`"
-
-# Backup Variables
-VBND="$BASE/$DATE"
-DBMN="$HOME/backups/$VBND"
-
-# Working Variables
-DWMN="/tmp/$VBND"
-FWTO="$DWMN/tempone.tmp"
-FWTT="$DWMN/temptwo.tmp"
-
-# Custom Variables
-
 # Functions
 ########################################
 
@@ -31,20 +14,9 @@ f_hlp () { echo "
 DESCRIPTION:
   This script ...
 
-SYNTAX:
-  # $BASE [OPTIONS]
-
 OPTIONS:
-  -h, --help
-    Displays this help text.
-  -r, --run
-    Executes this script.
-
-EXAMPLES:
-  # $BASE -h
-
-NOTES:
-  ...
+  -h, --help  Displays this help text.
+  -r, --run   Executes this script.
 " ; exit ; }
 
 # Print error message & help text
@@ -53,27 +25,15 @@ f_errr () { echo ;echo "!!! ERROR: $1 !!!" ;f_hlp ; }
 # If needed, make directory
 f_ifdir () { if [[ ! -d "$1" ]] ;then mkdir -p "$1" ;fi ; }
 
-# Initialize files/directories
-f_initdirs () {
-  f_ifdir "$DBMN"
-  f_ifdir "$DWMN"
-  cat /dev/null > $FWTO
-  cat /dev/null > $FWTT ; }
-
 # Print message with dots
-f_dots () {
-  echo ;echo -n "$1"
+f_dots () { echo ;echo -n "$1"
   for dots in {1..5} ;do
     sleep 0.2 ;echo -n '.' ;done ;echo ; }
 
 # Display variables
-f_display () {
-  f_initdirs
-  exit # need to check compatability on linux & mac
-  declare -p > $FWTO
-  for var in `grep '=' $FWTO |grep '^[A-Z]' |awk -F'=' '{print $1}'` ;do
-    if [[ `grep "$var" "$FLLP"` ]];then
-      grep ^$var\= $FWTO ;fi ;done ; }
+f_display () { echo "
+HOME=$HOME
+" ; }
 
 # Arguments
 ########################################
@@ -81,14 +41,7 @@ f_display () {
 # Filter options
 case $1 in
   '-r'|'--run')  RUN='true' ;shift  ;;
-  *)  HELP='true' ;shift  ;;
-esac
-
-# Processing
-################################################################################
-
-# Error Checks
-########################################
+  *)  HELP='true' ;shift  ;; ; esac
 
 # Check for help
 if [[ $HELP == 'true' ]] ;then f_hlp ;fi
@@ -96,8 +49,8 @@ if [[ $HELP == 'true' ]] ;then f_hlp ;fi
 # Check for root privilege execution
 #if [[ `whoami` != 'root' ]] ;then f_errr 'use "sudo" for execution' ;fi
 
-# Script Start
-########################################
+# Processing (Script Start)
+################################################################################
 
 # Process options
 if [[ $RUN != 'true' ]] ;then
